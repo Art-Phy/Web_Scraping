7479292f7a8e7c7f3a3544653dba79776cb3b013
ArturoGit <arturosanma@gmail.com>
2025-11-06 17:43:03 +0100

    LICENSE actualizada a MIT

--- scraping.py
+++ scraping.py
@@ -1,35 +1,19 @@
-### Web Scraping ###
+"""
+======================
+WEB SCRAPING (V1.1.0)
+======================
 
-# Importamos los módulos necesarios
+Script genérico para realizar Web Scraping y listar los texto de los enlaces <a> 
+que contengan la palabra clave indicada por el usuario.
+
+Autor: Art-Phy
+Require: requests, beatifulsoup4
+"""
+
 import requests
-from bs4 import BeautifulSoup
+from beautifulsoup4 import BeautifulSoup
+import argparse
+from pathlib import Path
 
-# Indicamos la URL de la web que queramos
-url = "https://valenciasecreta.com/restaurantes-valencia/"
-
-# Añadimos get
-respuesta = requests.get(url)
-
-# Comprobamos si la petición funcionó
-if respuesta.status_code == 200:
-    # Creamos una instancia con el contenido de la página
-    soup = BeautifulSoup(respuesta.content, "html.parser")
-
-    # Le decimos que encuentre los enlaces <a> que pueden contener los nombres de los restaurantes
-    restaurantes = soup.find_all('a')
-
-    # Creamos una lista para almacenar los nombres de los restaurantes
-    restaurante_lista = []
-
-    # Iteramos sobre los enlaces y extrae el texto asociado a nuestra palabra
-    for restaurante in restaurantes:
-        if restaurante.get_text():
-            if "restaurante" in restaurante.get_text().lower():
-                restaurante_lista.append(restaurante.get_text().strip())
-        
-    # Muestra los resultados
-    print("\nLos restaurantes encontrados son: ")
-    for i, rest in enumerate(restaurante_lista, 1):
-        print(f"{i}. {rest}")
-else:
-    print(f"Algo no ha ido bien. {respuesta.status_code}")
+def extraer_textos(url: str, palabra: str) ->list[str]:
+    
